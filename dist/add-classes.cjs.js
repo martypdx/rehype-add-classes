3111de8a0a5e7f921bb23bc863b45cd3df2be782
Tyler Coleman <tyler@tmbp.local>
2022-06-19 15:05:40 -0700

    Update add classes to ESM

--- dist/add-classes.cjs.js
+++ dist/add-classes.cjs.js
@@ -1,40 +1,32 @@
-'use strict';
+import selector from 'hast-util-select';
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+const { selectAll } = selector;
 
-import('hast-util-select')
-    .then((module) => {
-        var selector = _interopDefault(module);
-
-        var selectAll = selector.selectAll;
-        
-        
-        var index = (function (additions) {
-            var adders = Object.entries(additions).map(adder);
-            return function (node) {
-                return adders.forEach(function (a) {
-                    return a(node);
-                });
-            };
+var index = (function (additions) {
+    var adders = Object.entries(additions).map(adder);
+    return function (node) {
+        return adders.forEach(function (a) {
+            return a(node);
         });
-        
-        var adder = function adder(_ref) {
-            var selector$$1 = _ref[0],
-                className = _ref[1];
-        
-            var writer = write(className);
-            return function (node) {
-                return selectAll(selector$$1, node).forEach(writer);
-            };
-        };
-        
-        var write = function write(className) {
-            return function (_ref2) {
-                var properties = _ref2.properties;
-        
-                if (!properties.className) properties.className = className;else properties.className += ' ' + className;
-            };
-        };
-        
-        module.exports = index;
-    });
+    };
+});
+
+var adder = function adder(_ref) {
+    var selector$$1 = _ref[0],
+        className = _ref[1];
+
+    var writer = write(className);
+    return function (node) {
+        return selectAll(selector$$1, node).forEach(writer);
+    };
+};
+
+var write = function write(className) {
+    return function (_ref2) {
+        var properties = _ref2.properties;
+
+        if (!properties.className) properties.className = className;else properties.className += ' ' + className;
+    };
+};
+
+export default index;
